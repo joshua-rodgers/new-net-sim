a9c115cd8d4d5f7237814ae4e9132c9217a81753
Joshua Rodgers-AR River <52717232+rodgersjaresc@users.noreply.github.com>
2019-09-12 15:09:06 -0500

    updated my knowledge of JS encapsulation
    Finally understood more clearly how to encapsulate a member of an object. declaring _output_text in the parameter list of the object was not encapsulating it. Not sure how I ever felt it was. I understand closure somewhat better now and recognize that much info online could be improved to say the least. Renamed Output.send_output method to write_output. Rewrote the mothod for clarity as I noticed that some of its functionality fell into a category of its own. It is now neatly divided into three areas of concern: 1. moving the active line down, 2. updating the contents of the screen_buffer, and 3. reinitializing the terminal to allow further input.

--- terminal.js
+++ terminal.js
@@ -26,23 +26,34 @@ function init_terminal(){
 
 /*
 * The Output object hides the _output_text Array to prevent accidental modification of the buffer
-* As _output_text is defined in the parameter list, it is only accessible within the object itself
-* not even via the prototype chain.
-*
+* As _output_text is not defined on the prototype chain (defined with 'var' not 'this'), it is 
+* only accessible within the object itself.
+*.
 *
 *
 *
 */
-function Output(_output_text = []){
+function Output(){
+    var _output_text = [];
+    
     this.build_output = function (input) {
         _output_text.push(input);
     }
-    this.send_output = function () {
+    
+    var _move_active_line = function () {
         terminal[active_line].innerHTML = "&nbsp";  // clear current line
         terminal[active_line].removeEventListener("keypress", new_line_handler);  // remove "ENTER" key listener
         active_line = 0;  // this and next line tell us how far to move active line down to make room for output
         active_line += _output_text.length;
-
+    }
+    
+    var _reinit_terminal = function () {
+        terminal[active_line].innerHTML = prompt;
+        terminal[active_line].addEventListener("keypress", new_line_handler);                                                          
+        document.getElementById("al").focus();
+    }
+    
+    var _update_screen_buffer = function () {
         for(a of terminal.keys()){  // .keys() just  gives us terminal[]'s indexes to iterate with
             if(a < active_line){ // lines above active line
                 if(_output_text[a] == ""){  // blank line submitted?
@@ -54,22 +65,22 @@ function Output(_output_text = []){
                 terminal[a].innerHTML = "&nbsp";
             }  
         }
-
-        terminal[active_line].innerHTML = prompt;
-        terminal[active_line].addEventListener("keypress", new_line_handler);                                                          
-        document.getElementById("al").focus();
-
+    }
+    this.write_output = function () {
+        _move_active_line();
+        _update_screen_buffer();
+        _reinit_terminal();
     }
     this.clear_buffer = function (ok_to_clear = false) {  // default parameter value requires explicit passing of 'true'
         if(ok_to_clear){
             _output_text = [];  // there are no other references to _output_text, so this is safe and discards old array
-            this.send_output();  // actually 'commits' wipe to screen
+            this.write_output();  // actually 'commits' wipe to screen
         }
     }
 }
 
 // These declarations create the prompt. They're down here because the terminal 
-// dynamically generated and is only now ready to be manipulated. 'context' will
+// is dynamically generated and is only now ready to be manipulated. 'context' will
 // change as its name suggests
 var context = "<span contentEditable=false>>&nbsp</span>";
 var line = "<span id='al' contentEditable=true></span>";
@@ -82,7 +93,7 @@ function submitLine(){
         screen_buffer.clear_buffer(true);
     }else {
         screen_buffer.build_output(submitted_text);
-        screen_buffer.send_output();
+        screen_buffer.write_output();
     }
 }
 
